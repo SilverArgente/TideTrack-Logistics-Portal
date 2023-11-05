f0512b8fedc1c8b155a8b5010cebe2a338afabf6
silver <53583492+SilverArgente@users.noreply.github.com>
2023-11-05 07:42:34 -0500

    Updated delete button to rerender component

--- tide_track/src/components/edit_routes/EditRoutes.js
+++ tide_track/src/components/edit_routes/EditRoutes.js
@@ -1,30 +1,27 @@
-import React, {useState} from "react";
-import {useEffect} from "react";
+import React, { useState } from "react";
 import '../../css/EditRoutes.css';
 
-function EditRoutes( {backToPortal, routes} ) {
-
-    console.log(routes[0].shipping_id);
+function EditRoutes({ backToPortal, routes }) {
+    const [rerender, setRerender] = useState(0); // Initialize with 0
+    const [updatedRoutes, setUpdatedRoutes] = useState(routes); // Manage routes
 
     function addRoutes() {
         let modules = [];
-        for (let i in routes) {
+        for (let i in updatedRoutes) {
             modules.push(
                 <div key={i} className="route-bar">
-                    <div className="value">{routes[i].shipping_id}</div>
-                    <div className="value">{routes[i].start_date}</div>
-                    <div className="value">{routes[i].arrival_date}</div>
-                    <div className="value">{routes[i].source}</div>
-                    <div className="value">{routes[i].destination}</div>
-                    <div className="value">{routes[i].shipping_weight}</div>
-                    <div className="value">{routes[i].cost}</div>
-                    <button className="delete-button" onClick={() => handleDeleteColumn(routes[i].shipping_id)}>X</button>
+                    <div className="value">{updatedRoutes[i].shipping_id}</div>
+                    <div className="value">{updatedRoutes[i].start_date}</div>
+                    <div className="value">{updatedRoutes[i].arrival_date}</div>
+                    <div className="value">{updatedRoutes[i].source}</div>
+                    <div className="value">{updatedRoutes[i].destination}</div>
+                    <div className="value">{updatedRoutes[i].shipping_weight}</div>
+                    <div className="value">{updatedRoutes[i].cost}</div>
+                    <button className="delete-button" onClick={() => handleDeleteColumn(updatedRoutes[i].shipping_id)}>X</button>
                 </div>
             );
         }
         return modules;
-
-
     }
 
     function handleDeleteColumn(id) {
@@ -32,35 +29,30 @@ function EditRoutes( {backToPortal, routes} ) {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
-                // Add any other headers you may need (e.g., authentication headers)
             },
         })
             .then(response => {
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    throw Error('Network response was not ok');
                 }
                 return response.json();
             })
             .then(data => {
                 console.log('Column deleted successfully', data);
-                // If needed, update the state or perform any other actions after successful deletion
+                // Update the routes to exclude the deleted entry
+                setUpdatedRoutes(updatedRoutes.filter(route => route.shipping_id !== id));
             })
             .catch(error => {
                 console.error('Error deleting column:', error);
-                // Handle errors (e.g., show an error message to the user)
             });
     }
-
-
 
     return (
         <div className="main-container">
             <div className="title"><b>Edit Routes</b></div>
-
             <div className="columns">
                 <div className="current-routes">
                     <p>Edit Current Routes</p>
-
                     <div className="route-bar">
                         <div className="value-description">Shipping ID</div>
                         <div className="value-description">Start Date</div>
@@ -71,20 +63,15 @@ function EditRoutes( {backToPortal, routes} ) {
                         <div className="value-description">Cost</div>
                         <div className="value-description">Delete</div>
                     </div>
-
                     <div>{addRoutes()}</div>
                 </div>
-
             </div>
-
             <div className="footer">
                 <button type="button">Create Route</button>
                 <button type="button" onClick={backToPortal}>Back to Portal</button>
             </div>
-
         </div>
     );
-
 }
 
 export default EditRoutes;
