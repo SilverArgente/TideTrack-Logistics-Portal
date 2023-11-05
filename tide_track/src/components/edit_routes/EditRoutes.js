2e6aae0446131904bfc299279e647507d2fb02f2
silver <53583492+SilverArgente@users.noreply.github.com>
2023-11-04 20:03:12 -0400

    Added database fetch code snippet

--- tide_track/src/components/edit_routes/EditRoutes.js
+++ tide_track/src/components/edit_routes/EditRoutes.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
+import {useEffect} from "react";
 
 function EditRoutes( {backToPortal} ) {
+
+    const [routeInfo, setRouteInfo] = useState([[]]);
+
     return (
         <div className="main-container">
 
--- tide_track/src/components/portal/MainContainer.js
+++ tide_track/src/components/portal/MainContainer.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "../../css/MainContainer.css";
 
 function MainContainer( { changeToShippingHistory, changeToEditRoutes } ) {
+
+    useEffect(() => {
+        fetch('http://159.89.252.189:3000/routes')
+            .then((res) => res.json())
+            .then((data) => {
+
+                console.log(data);
+                // setRouteInfo(data.map(current) => current.);
+
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
 
     return (
         <div class="main-container">
